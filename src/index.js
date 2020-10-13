81856454fba2f2ddffa391e8b493e27f8a0acf33
itay <itayshetrit@gmail.com>
2020-10-13 16:02:22 +0300

    18

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const cors = require('cors')
 const usersRouter = require('./routes/users/index.js')
 const guestRouter = require('./routes/guests/index.js')
 const tablesRouter = require('./routes/tables/index.js')
-// const userRouter = require('./routers/user')
-// const turnRouter = require('./routers/turn')
+
 const app = experss()
 const port = process.env.PORT || 8000
 app.use(cors())
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://be-simple-api.herokuapp.com/");
+    next();
+});
 app.use(experss.json())
-// app.use(userRouter)
 app.use(usersRouter)
 app.use(guestRouter)
 app.use(tablesRouter)
