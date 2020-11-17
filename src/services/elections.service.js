bc7ede8ab9a64428b2bc2887ba39c40f300cf937
itayshetrit <itayshetrit@gmail.com>
2020-11-17 02:39:04 +0200

    gal has to check

--- src/services/elections.service.js
+++ src/services/elections.service.js
@@ -3,14 +3,13 @@ import { responseWrapper, responseSuccess } from '../common/respone';
 export const elections = async (req) => {
 	console.log("elections service");
 	try {
-		const {list,numOfTeams} = {...req.body};
+		const { list, numOfTeams } = { ...req.body };
 		//console.log(list)
 		const numberOfPlayers = list.length
 		//console.log("numberOfPlayers",numberOfPlayers)
-		if(numberOfPlayers % numOfTeams !=0 )
-		{
+		if (numberOfPlayers % numOfTeams != 0) {
 			// TODO: need to rerurn error
-			return responseSuccess({ ok: 1 })
+			return responseWrapper(500, { error: "מספר השחקנים שנבחרו לא תקין" });
 		}
 		const numberOfPlayersInTeam = numberOfPlayers / numOfTeams
 		//console.log("numberOfPlayersInTeam",numberOfPlayersInTeam)
@@ -32,35 +31,34 @@ export const elections = async (req) => {
 
 			//Index = 2 will be flag if we are chooshe player to this team or not
 			teams[i][2] = false
-			
+
 		}
-		var random= Math.floor(Math.random() * 2);
-		console.log("random",random)
+		var random = Math.floor(Math.random() * 2);
+		console.log("random", random)
 		var isAttack = true
-		if(random==0)
-		{
+		if (random == 0) {
 			isAttack = false;
 		}
 
-		while(orderingByAttack.length !=0 || orderingByDefense !=0){
-			if(isAttack && orderingByAttack.length !=0 ){
-				console.log("isAttack",isAttack)
-				roundMaker(numOfTeams,orderingByAttack,orderingByDefense,teams,isAttack)
+		while (orderingByAttack.length != 0 || orderingByDefense != 0) {
+			if (isAttack && orderingByAttack.length != 0) {
+				console.log("isAttack", isAttack)
+				roundMaker(numOfTeams, orderingByAttack, orderingByDefense, teams, isAttack)
 				isAttack = false
 			}
-			else if (!isAttack && orderingByDefense.length !=0){
-				console.log("isAttack",isAttack)
-				roundMaker(numOfTeams,orderingByDefense,orderingByAttack,teams,isAttack)
+			else if (!isAttack && orderingByDefense.length != 0) {
+				console.log("isAttack", isAttack)
+				roundMaker(numOfTeams, orderingByDefense, orderingByAttack, teams, isAttack)
 				isAttack = true
 			}
 		}
 
 		//TODO: need to remove
-		for(var i = 0; i < numOfTeams; i++){
+		for (var i = 0; i < numOfTeams; i++) {
 			console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-			console.log("Teams"+i)
-			for(var j = 0; j < teams[i][0].length; j++){
-				console.log("player "+j +": "  ,teams[i][0][j].fname)
+			console.log("Teams" + i)
+			for (var j = 0; j < teams[i][0].length; j++) {
+				console.log("player " + j + ": ", teams[i][0][j].fname)
 			}
 		}
 
@@ -71,22 +69,22 @@ export const elections = async (req) => {
 	}
 }
 
-var roundMaker = function ( numOfTeams, listForShift, listForRemove,teams,isAttack ) {
+var roundMaker = function (numOfTeams, listForShift, listForRemove, teams, isAttack) {
 
 	const playersArray = []
-	for(var j = 0; j < numOfTeams; j++){
+	for (var j = 0; j < numOfTeams; j++) {
 		playersArray.push(listForShift.shift())
 	}
-	playersArray.sort((a, b) => ((parseInt(b.attack)+parseInt(b.defense)/2) - (parseInt(a.attack)+parseInt(a.defense)/2))).slice() 
+	playersArray.sort((a, b) => ((parseInt(b.attack) + parseInt(b.defense) / 2) - (parseInt(a.attack) + parseInt(a.defense) / 2))).slice()
 	//TODO: need to fix the issue when we have the same average we need to tae the player with higher attack/defense		
 	// || isAttack ? parseInt(a.attack) - parseInt(b.attack) : parseInt(a.defense) - parseInt(b.defense)).slice()
-	console.log("playersArray",playersArray)
+	console.log("playersArray", playersArray)
 
-	for(var i = 0; i < numOfTeams; i++){
+	for (var i = 0; i < numOfTeams; i++) {
 		const player = playersArray.shift()
-		var teamWithMinScore = getTeamWithMinScore(teams,numOfTeams)
+		var teamWithMinScore = getTeamWithMinScore(teams, numOfTeams)
 		// TODO: get the best of top 3 base on attack and defense average
-		console.log("player",player.fname)
+		console.log("player", player.fname)
 
 		console.log("--------------------------------------------------------------")
 		const indexOfPlayerInListForRemove = listForRemove.indexOf(player);
@@ -105,27 +103,25 @@ var roundMaker = function ( numOfTeams, listForShift, listForRemove,teams,isAtta
 
 	}
 
-	for(var i = 0; i < numOfTeams; i++){
+	for (var i = 0; i < numOfTeams; i++) {
 		teams[i][2] = false
 	}
 }
 
-var getTeamWithMinScore = function ( teams,numOfTeams  ) {
+var getTeamWithMinScore = function (teams, numOfTeams) {
 	var minScore = Number.MAX_VALUE
 	var minIndex = new Array()
-	for(var i = 0; i < numOfTeams; i++){
-		if(teams[i][2] == false){
-			if(teams[i][0].length == 0)
-			{
+	for (var i = 0; i < numOfTeams; i++) {
+		if (teams[i][2] == false) {
+			if (teams[i][0].length == 0) {
 				minIndex.push(i)
 			}
-			else{
+			else {
 				var teamAverageScore = teams[i][1] / teams[i][0].length;
-				if(teamAverageScore == minScore){
+				if (teamAverageScore == minScore) {
 					minIndex.push(i)
 				}
-				else if(teamAverageScore < minScore)
-				{
+				else if (teamAverageScore < minScore) {
 					minScore = teamAverageScore;
 					minIndex = []
 					minIndex.push(i)
@@ -133,12 +129,12 @@ var getTeamWithMinScore = function ( teams,numOfTeams  ) {
 			}
 		}
 	}
-	console.log("minScore",minScore)
-	var random= Math.floor(Math.random() * minIndex.length);
-	console.log("Index min team ",minIndex[random])
-	console.log("min index array ",minIndex)
+	console.log("minScore", minScore)
+	var random = Math.floor(Math.random() * minIndex.length);
+	console.log("Index min team ", minIndex[random])
+	console.log("min index array ", minIndex)
 	return minIndex[random];
 
-//	return {elections:[[],[],[]], laundry:["gal","itay","..."]}
-	
+	//	return {elections:[[],[],[]], laundry:["gal","itay","..."]}
+
 }
